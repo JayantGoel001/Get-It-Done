ccb8ec9059a0f16ff6dd28fe0541a154e51baf0b
Cormac Krum <54479676+CormacKrum@users.noreply.github.com>
2020-07-25 12:16:11 +0530

    Add files via upload

---
+++ JavaScripts/date_format.js
@@ -0,0 +1,12 @@
+function getCurrentDate() {
+    let date = new Date();
+
+    let months = ['Jan.','Feb.','Mar.','Apr.','May','Jun.','Jul.','Aug.','Sept.','Oct.','Nov.','Dec.'];
+
+    let month = months[date.getMonth()];
+    let day = date.getDate();
+
+    
+
+}
+getCurrentDate();
